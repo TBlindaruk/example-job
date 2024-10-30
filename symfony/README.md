363ab3ded0b1cc025f1bf514318c95f758867506
Tetiana Blindaruk <t.blindaruk@gmail.com>
2024-10-30 22:36:40 +0200

    [symfony] Introduce full PUT /data

--- symfony/README.md
+++ symfony/README.md
@@ -1,5 +1,8 @@
 ### First
 
 Queues background job via Redis, to download and insert/upsert data in specified number of seconds:
-$ curl -s -X PUT 'http://127.0.0.1:8081/data?action=refresh&delaySeconds=123'
+curl -s -X PUT 'http://127.0.0.1:8081/data' \
+-H 'Content-Type: application/json' \
+-d '{"delaySeconds": 123}'
+
 {"data": {"success": true}}
--- symfony/app/src/Region/Http/Controller/Data/PutAction.php
+++ symfony/app/src/Region/Http/Controller/Data/PutAction.php
@@ -4,20 +4,26 @@ declare(strict_types = 1);
 namespace App\Region\Http\Controller\Data;
 
 use App\Region\Http\Requests\Data\DataPut;
+use App\Region\Worker\Message\MapUpdate;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\DelayStamp;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PutAction
 {
-//    public function __construct(
-//        private readonly DataScheduleExecutor $dataScheduleExecutor,
-//    ) {
-//    }
-
     #[Route('/data', name: 'data_put', methods: 'PUT', format: 'json')]
-    public function __invoke(#[MapRequestPayload] DataPut $dataPut): JsonResponse
+    public function __invoke(
+        #[MapRequestPayload] DataPut $dataPut,
+        MessageBusInterface $messageBus
+    ): JsonResponse
     {
+        $messageBus->dispatch(
+            new MapUpdate(),
+            [new DelayStamp($dataPut->getDelaySeconds())]
+        );
+
         return new JsonResponse( // TODO: improve it - try to create a separate DTO
             [
                 'data' => [
